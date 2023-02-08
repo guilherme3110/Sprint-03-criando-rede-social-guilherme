f24f983a425aef916e247f7d080dbc4f84103bf7
guilherme3110 <guilherme311094@hotmail.com>
2023-02-08 15:06:46 -0300

    começando a estilizar pagina

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
-import { posts, users } from "./database";
+import { posts, suggestUsers, users } from "./database.js";
 
-function renderUser(users, posts){
+function renderPosts(){
     const secaoCard = document.querySelector('.list__container')
-    array.forEach((users, posts) => {
-        let cardLi = document.createElement('li')
+    for( let i = 0; i < posts.length; i++) {
+        const cardLi = document.createElement('li')
         const img = document.createElement('img')
         const h2 = document.createElement('h2')
         const p = document.createElement('p')
@@ -14,21 +14,48 @@ function renderUser(users, posts){
 
         pTitle.innerText = posts[i].title
         pText.innerText = posts[i].text
-        carLi.id = posts[i].id_post
+        cardLi.id = posts[i].id_post
 
 
-        img.src = users[img]
+        img.src = posts[i].img
         h2.classList.add = 'usuarios'
-        h2.innerText = users[i].user
-        p.classList.add = users[i].stack
-        p.innerText = users[i].stack
+        h2.innerText = posts[i].user
+        p.classList.add = posts[i].stack
+        p.innerText = posts[i].stack
         btn.addEventListener('click', () => createModal(users[i], posts[i]))
-        btnImage.innerText = 
+        btnImage.innerText = ''
 
         cardLi.append(img, h2, p, pTitle, pText, btn)
         secaoCard.appendChild(cardLi)
 
-    });
+    };
 }
+renderPosts(posts)
 
-renderUser(users, posts)
+function renderSuggestUsers(){
+    const secaoAside = document.querySelector('.aside__suggestUsers')
+     for(let i = 0 ; i < suggestUsers.length; i++){
+        const cardLi = document.createElement('li')
+        const h2 = document.createElement('h2')
+        const p = document.createElement('p')
+        const img = document.createElement('img')
+
+        h2.innerText = suggestUsers.user
+        p.innerText = suggestUsers.stack
+        img.src = suggestUsers.img
+
+        cardLi.append(img, h2, p)
+        secaoAside.appendChild(cardLi)
+
+    };
+}
+renderSuggestUsers(suggestUsers)
+
+ function renderUser(){
+    const sectionPrimary = document.querySelector('.user__config')
+    for (let index = 0; index < users.length; index++) {
+        
+        
+        
+    }
+ }
